9f2677177f8d0fc58b4fa68661df5d766f0e59e0
goferville <goferville@gmail.com>
2018-04-02 22:26:47 -0700

    add app folder

---
+++ py_pdfminer_1.py
@@ -0,0 +1,37 @@
+"""
+I have MS Visual Studio in this PC
+then "pip3 install pdfminer.six
+"""
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import pdfminer
+import io
+from io import StringIO
+# The StringIO and cStringIO modules are gone. Instead, import the io module and use io.StringIO or io.BytesIO for text and data respectively.
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+pdfFile="document.pdf"
+print(convert_pdf_to_txt(pdfFile))
